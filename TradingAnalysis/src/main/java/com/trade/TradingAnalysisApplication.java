ce8c44b85dc64fd60fcba73bbc0d62246f0254c3
USER <USER@DESKTOP-VEJB4IF>
2020-06-13 08:40:37 +0530

    First Commit

---
+++ TradingAnalysis/src/main/java/com/trade/TradingAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAnalysisApplication.class, args);
+	}
+
+}
