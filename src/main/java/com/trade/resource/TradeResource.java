37e4d2f7b8e942c1af31b818ed4e1f3a451d9c0e
USER <srihith0184@gmail.com>
2020-06-24 09:26:23 +0530

    committing the latest changes

--- src/main/java/com/trade/resource/TradeResource.java
+++ src/main/java/com/trade/resource/TradeResource.java
@@ -6,18 +6,20 @@ import javax.ws.rs.QueryParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.trade.api.ParentMostActiveCallPutAll;
 import com.trade.api.DataPoints;
 import com.trade.api.ParentBankNiftyFuture;
+import com.trade.api.ParentMostActiveCallPutAll;
 import com.trade.api.TradeSymbol;
 import com.trade.service.BankNiftyService;
 import com.trade.service.StockService;
 import com.trade.service.TradeService;
 
+@CrossOrigin(origins = "https://www1.nseindia.com", maxAge = 3600)
 @Controller
 @RequestMapping("/trading")
 public class TradeResource {
